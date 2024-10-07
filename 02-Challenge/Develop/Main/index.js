b3e043fa5df94449c488d8b5c75262eb65668d4f
jwilson0213 <jeffwils0723@gmail.com>
2024-10-06 20:27:00 -0400

    Updated index.js, generateMarkdown.js

--- 02-Challenge/Develop/Main/index.js
+++ 02-Challenge/Develop/Main/index.js
@@ -37,8 +37,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
     fs.writeFile('README.md', questions )
+}
 // TODO: Create a function to initialize app
 function init() {}
 
